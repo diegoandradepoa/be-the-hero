ce8360c52f249e85d42ebe02b265bfc172fbe57a
Diego Andrade <diegoandradecruz15@gmail.com>
2020-04-17 14:54:24 -0300

    Corrigido delete = Back-end da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -50,10 +50,11 @@ module.exports = {
       if (incident.ong_id != ong_id) {
           return response.status(401).json({ error: 'Operation not permitted.' });
         }
-
-       /*await*/ connection('incidents').where('id,', id).delete();
+        
+       await connection('incidents').where('id', id).delete();
 
         return response.status(204).send();
+      
     }
 };
 
